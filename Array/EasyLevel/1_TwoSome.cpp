a6dc2992712e5f688d5bafda487adc706c31b224
aniketsardar47 <aniketsardar89@gmail.com>
2024-06-17 19:58:08 +0530

    First Problem

--- Array/EasyLevel/1_TwoSome.cpp
+++ Array/EasyLevel/1_TwoSome.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+     int size = nums.size();
+      int i = 0;
+        for(int i=0;i<size;i++){
+            for(int j=i+1;j<size;j++){
+                if(nums[i]+nums[j] == target){
+                    return {i,j};
+                }
+            }
+        }
+        return {};
+        
+    }
+    
+};
 
-int main()
-{
-    std::cout << "Hello, world!";
-    return 0;
-}
+
+
