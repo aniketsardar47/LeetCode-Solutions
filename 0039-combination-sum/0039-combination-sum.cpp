28bcbce7b8e65492d0aaf7612a5f95a96ce8f448
_.anik.ett._7 <106902733+aniketsardar47@users.noreply.github.com>
2026-05-29 10:36:39 +0530

    Time: 71 ms (8.9%), Space: 63.3 MB (8.21%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    void rfunc(vector<int>& candidates, int target,vector<int> curr, int sum){
-        if(sum == target){
-            sort(curr.begin(),curr.end());
-            if(find(ans.begin(),ans.end(),curr) == ans.end())
+    void rfunc(int i,int n,vector<int>& candidates, int target,vector<int> curr, int sum){
+        if(i==n){
+            if(sum==target)
                 ans.push_back(curr);
             return;
         }
 
-        if(sum < target)
-        {
-            for(int candidate: candidates){
-                if(sum+candidate <= target){
-                    curr.push_back(candidate);
-                    rfunc(candidates,target,curr,sum+candidate);
-                    curr.pop_back();
-                }
-            }
+        rfunc(i+1,n,candidates,target,curr,sum);
+        curr.push_back(candidates[i]);
+        if(sum+candidates[i] <= target){
+            rfunc(i,n,candidates,target,curr,sum+candidates[i]);
         }
 
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int> curr;
-        rfunc(candidates,target,curr,0);
+        rfunc(0,candidates.size(),candidates,target,curr,0);
 
         return ans;
     }
