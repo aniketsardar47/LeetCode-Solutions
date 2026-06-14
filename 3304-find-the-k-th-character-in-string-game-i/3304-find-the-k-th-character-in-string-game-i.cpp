5d04d55303569d907a2f514709027dbc57715f25
_.anik.ett._7 <106902733+aniketsardar47@users.noreply.github.com>
2026-06-14 12:16:45 +0530

    Time: 0 ms (100%), Space: 7.6 MB (99.3%) - LeetHub

--- 3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.cpp
+++ 3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.cpp
@@ -1,21 +1,7 @@
 class Solution {
 public:
-    char rfunc(string str,int k){
-        if(str.size() >= k){
-            return str[k-1];
-        }
-
-        string temp = "";
-        for(auto ch: str){
-            temp += 'a' + (ch - 'a' + 1) % 26;
-
-        }
-
-        return rfunc(str+temp,k);
-
-    }
-
     char kthCharacter(int k) {
-        return rfunc("a", k);
+        int shifts = __builtin_popcount(k - 1);
+        return 'a' + (shifts % 26);
     }
 };
